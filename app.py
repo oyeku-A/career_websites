a4e2481f87ce614ce054e415cc70805cd0df86b8
oyeku-A <82169558+oyeku-A@users.noreply.github.com>
2023-08-18 15:51:18 +0000

    version 1

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'A, B',
+  'salary': '200'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'B, C',
+}, {
+  'id': 3,
+  'title': 'Data Engineer',
+  'location': 'C, D',
+  'salary': '300'
+}, {
+  'id': 4,
+  'title': 'Data Specialist',
+  'location': 'D, E',
+  'salary': '350'
+}]
+
+
 @app.route("/")
-def hello_world():
-    return "Hello, World!!!"
+def home_page():
+  return render_template('index.html', jobs=JOBS)
 
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug = True )
+  app.run(host='0.0.0.0', debug=True)
